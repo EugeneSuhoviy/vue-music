6258dca7649b96e8306c73bbf918584ee9920a1f
EugeneSuhoviy <eugene.s@overdose.digital>
2024-01-05 11:13:31 +0200

    Added routers

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,11 +3,21 @@
     <header id="header" class="bg-gray-700">
         <nav class="container mx-auto flex justify-start items-center py-5 px-4">
             <!-- App Name -->
-            <a class="text-white font-bold uppercase text-2xl mr-4" href="#">Music</a>
+            <router-link
+                class="text-white font-bold uppercase text-2xl mr-4"
+                :to="{ name: 'home' }"
+                exact-active-class="no-active"
+                >Music
+            </router-link>
 
             <div class="flex flex-grow items-center">
                 <!-- Primary Navigation -->
                 <ul class="flex flex-row mt-1">
+                    <li>
+                        <router-link class="px-2 text-white" :to="{ name: 'about' }"
+                            >About</router-link
+                        >
+                    </li>
                     <!-- Navigation Links -->
                     <li v-if="!userStore.userLoggedIn">
                         <a class="px-2 text-white" href="#" @click.prevent="toggleAuthModal"
@@ -16,10 +26,12 @@
                     </li>
                     <template v-else>
                         <li>
-                            <a class="px-2 text-white" href="#">Manage</a>
+                            <router-link class="px-2 text-white" :to="{ name: 'manage' }"
+                                >Manage</router-link
+                            >
                         </li>
                         <li>
-                            <a class="px-2 text-white" href="#" @click.prevent="userStore.signOut">Logout</a>
+                            <a class="px-2 text-white" href="#" @click.prevent="signOut">Logout</a>
                         </li>
                     </template>
                 </ul>
@@ -40,6 +52,14 @@ export default {
     methods: {
         toggleAuthModal() {
             this.modalStore.isOpen = !this.modalStore.isOpen;
+        },
+        signOut() {
+            this.userStore.signOut();
+
+            // if (this.$router.name === 'manage') meta fields
+            if (this.$router.meta.requiresAuth) {
+                this.$router.push({ name: 'home' });
+            }
         }
     }
 };
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,62 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
+import Home from '@/views/Home.vue';
+import About from '@/views/About.vue';
+import Manage from '@/views/Manage.vue';
+import useUserStore from '@/stores/user';
 
-const routes = []
+const routes = [
+    {
+        name: 'home', // alias for templates
+        path: '/',
+        component: Home
+    },
+    {
+        name: 'about',
+        path: '/about',
+        component: About
+    },
+    {
+        name: 'manage',
+        // alias: '/manage',
+        path: '/manage-music',
+        component: Manage,
+        beforeEnter: (to, from, next) => {
+            next();
+        },
+        meta: {
+            requiresAuth: true
+        }
+    },
+    {
+        path: '/manage',
+        redirect: { name: 'manage' }
+    },
+    {
+        path: '/:catchAll(.*)*',
+        redirect: { name: 'home' }
+    }
+];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes
-})
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes,
+    linkExactActiveClass: 'text-yellow-500'
+});
 
-export default router
+router.beforeEach((to, from, next) => {
+    // debugger;
+    if (!to.meta.requiresAuth) {
+        next();
+        return;
+    }
+
+    const store = useUserStore();
+
+    if (store.userLoggedIn) {
+        next();
+    } else {
+        next({ name: 'home' });
+    }
+}); // for routers guard
+
+export default router;
