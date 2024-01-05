6115740f0de82cc74da0d1c1e24dbbaa4794ab8e
EugeneSuhoviy <eugene.s@overdose.digital>
2024-01-05 18:20:45 +0200

    Updated assets

--- src/includes/firebase.js
+++ src/includes/firebase.js
@@ -1,6 +1,7 @@
 import  firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
+import 'firebase/storage';
 
 const firebaseConfig = {
     apiKey: "AIzaSyDIj4ROh98FxjmGbjjJIBVEw0fB8Vc-xDc",
@@ -14,11 +15,13 @@ firebase.initializeApp(firebaseConfig);
 
 const auth = firebase.auth();
 const db = firebase.firestore();
+const storage = firebase.storage();
 
 const usersCollection = db.collection('users');
 
 export {
     auth,
     db,
-    usersCollection
+    usersCollection,
+    storage
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,7 @@
     <section class="mb-8 py-20 text-white text-center relative">
         <div
             class="absolute inset-0 w-full h-full bg-contain introduction-bg"
-            style="background-image: url(assets/img/header.png)"
+            style="background-image: url(../assets/img/header.png)"
         ></div>
         <div class="container mx-auto">
             <div class="text-white main-header-content">
@@ -19,7 +19,7 @@
 
         <img
             class="relative block mx-auto mt-5 -mb-20 w-auto max-w-full"
-            src="../../assets/img/introduction-music.png"
+            src="../assets/img/introduction-music.png"
         />
     </section>
 
